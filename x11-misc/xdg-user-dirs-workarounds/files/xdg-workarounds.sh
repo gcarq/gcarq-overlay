8e0ffd653df735a2f0aa565f7eaccb8e394d3c2b
gcarq <michael.egger@tsn.at>
2020-05-28 13:42:29 +0200

    x11-misc/xdg-user-dirs-workarounds: new package

---
+++ x11-misc/xdg-user-dirs-workarounds/files/xdg-workarounds.sh
@@ -0,0 +1,25 @@
+# Custom script to set XDG directories
+# See https://wiki.archlinux.org/index.php/XDG_Base_Directory
+
+# Base XDG directories
+export XDG_CONFIG_HOME="${HOME}/.config"
+export XDG_CACHE_HOME="${HOME}/.cache"
+export XDG_DATA_HOME="${HOME}/.local/share"
+
+# Program specific overrides
+export ANDROID_SDK_HOME="${XDG_CONFIG_HOME}/android"
+export ADB_VENDOR_KEY="${XDG_CONFIG_HOME}/android"
+export ATOM_HOME="${XDG_DATA_HOME}/atom"
+export CARGO_HOME="${XDG_DATA_HOME}/cargo"
+export DVDCSS_CACHE="${XDG_DATA_HOME}/dvdcss"
+export DOCKER_CONFIG="{$XDG_CONFIG_HOME}/docker"
+export ELECTRUMDIR="${XDG_DATA_HOME}/electrum"
+export GRADLE_USER_HOME="${XDG_DATA_HOME}/gradle"
+export GEM_HOME="${XDG_DATA_HOME}/gem"
+export GEM_SPEC_CACHE="${XDG_CACHE_HOME}/gem"
+export GNUPGHOME="${XDG_DATA_HOME}/gnupg"
+export RUSTUP_HOME="${XDG_DATA_HOME}/rustup"
+export VAGRANT_HOME="${XDG_DATA_HOME}/vagrant"
+export VAGRANT_ALIAS_FILE="${XDG_DATA_HOME}/vagrant/aliases"
+
+export _JAVA_OPTIONS="${_JAVA_OPTIONS} -Djava.util.prefs.userRoot=\"${XDG_CONFIG_HOME}/java\""
---
+++ x11-misc/xdg-user-dirs-workarounds/xdg-user-dirs-workarounds-0.1.ebuild
@@ -0,0 +1,18 @@
+# Copyright 1999-2020 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=7
+
+DESCRIPTION="Collection of XDG Base Directory workarounds"
+HOMEPAGE="https://github.com/gcarq/gcarq-overlay"
+KEYWORDS="~amd64 ~x86"
+
+SRC_URI=""
+
+LICENSE="GPL-3"
+SLOT="0"
+
+pkg_preinst() {
+	insinto /etc/profile.d
+	doins "${FILESDIR}/xdg-workarounds.sh"
+}
