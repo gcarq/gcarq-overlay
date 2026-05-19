12219f1ab78e9ded8f120d4cca9ee8cb03e1f5e1
gcarq <egger.m@protonmail.com>
2026-05-19 19:07:34 +0200

    x11-misc/xdg-user-dirs-workarounds: bump to 0.5.0
    Signed-off-by: gcarq <egger.m@protonmail.com>

--- x11-misc/xdg-user-dirs-workarounds/files/xdg-workarounds.sh
+++ x11-misc/xdg-user-dirs-workarounds/files/xdg-workarounds.sh
@@ -1,5 +1,9 @@
-# Custom script to set XDG directories
+# Custom script to set proper XDG directories for programs
+# that don't follow the XDG specification by default.
 # See https://wiki.archlinux.org/index.php/XDG_Base_Directory
+
+# Don't do anything if we are root
+[ "$(id -u)" -eq 0 ] && return 0
 
 # Base XDG directories
 [ -z "${XDG_CONFIG_HOME}" ] && export XDG_CONFIG_HOME="${HOME}/.config"
@@ -7,18 +11,18 @@
 [ -z "${XDG_DATA_HOME}" ] && export XDG_DATA_HOME="${HOME}/.local/share"
 
 # Program specific overrides
-export ANDROID_SDK_ROOT="${XDG_CONFIG_HOME}/android"
 export ADB_VENDOR_KEY="${XDG_CONFIG_HOME}/android"
-export ATOM_HOME="${XDG_DATA_HOME}/atom"
 export CARGO_HOME="${XDG_DATA_HOME}/cargo"
+export COPILOT_HOME="${XDG_CONFIG_HOME}/copilot"
 export DVDCSS_CACHE="${XDG_DATA_HOME}/dvdcss"
-export DOCKER_CONFIG="{$XDG_CONFIG_HOME}/docker"
+export DOCKER_CONFIG="${XDG_CONFIG_HOME}/docker"
 export ELECTRUMDIR="${XDG_DATA_HOME}/electrum"
 export GRADLE_USER_HOME="${XDG_DATA_HOME}/gradle"
 export GEM_HOME="${XDG_DATA_HOME}/gem"
 export GEM_SPEC_CACHE="${XDG_CACHE_HOME}/gem"
-export GNUPGHOME="${XDG_DATA_HOME}/gnupg"
+export GNUPGHOME="${XDG_CONFIG_HOME}/gnupg"
 export GOPATH="${XDG_DATA_HOME}/go"
+export GR_PREFS_PATH="${XDG_CONFIG_HOME}/gnuradio"
 export RUSTUP_HOME="${XDG_DATA_HOME}/rustup"
 export TEXMFHOME="${XDG_DATA_HOME}/texmf"
 export TEXMFVAR="${XDG_CACHE_HOME}/texlive/texmf-var"
@@ -26,4 +30,4 @@ export TEXMFCONFIG="${XDG_CONFIG_HOME}/texlive/texmf-config"
 export VAGRANT_HOME="${XDG_DATA_HOME}/vagrant"
 export VAGRANT_ALIAS_FILE="${XDG_DATA_HOME}/vagrant/aliases"
 
-export _JAVA_OPTIONS="${_JAVA_OPTIONS} -Djava.util.prefs.userRoot=\"${XDG_CONFIG_HOME}/java\""
+export _JAVA_OPTIONS="${_JAVA_OPTIONS} -Djava.util.prefs.userRoot='${XDG_CONFIG_HOME}/java'"
--- x11-misc/xdg-user-dirs-workarounds/xdg-user-dirs-workarounds-0.5.0.ebuild
+++ x11-misc/xdg-user-dirs-workarounds/xdg-user-dirs-workarounds-0.5.0.ebuild
@@ -1,18 +1,18 @@
-# Copyright 1999-2021 Gentoo Authors
+# Copyright 1999-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
-EAPI=7
+EAPI=8
 
 DESCRIPTION="Collection of XDG Base Directory workarounds"
 HOMEPAGE="https://github.com/gcarq/gcarq-overlay"
-KEYWORDS="~amd64 ~x86"
-
-SRC_URI=""
 
 LICENSE="GPL-3"
 SLOT="0"
+KEYWORDS="~amd64 ~x86"
 
-pkg_preinst() {
+S="${WORKDIR}"
+
+src_install() {
 	insinto /etc/profile.d
 	doins "${FILESDIR}/xdg-workarounds.sh"
 }
